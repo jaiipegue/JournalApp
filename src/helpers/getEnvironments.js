98a73f203227a54670665afb91fbdea476155101
Jaiipegue <jaiipegue12@gmail.com>
2022-10-17 17:09:19 -0500

    adjust environments

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,7 +1,10 @@
 export const getEnvironments = () => {
-  import.meta.env;
-
   return {
-    ...import.meta.env,
+    VITE_APIKEY: "AIzaSyAoKMUPrWC40Xlfxgfvxq8bEDI8gURUqHU",
+    VITE_AUTHDOMAIN: "react-cursos-94ad2.firebaseapp.com",
+    VITE_PROJECTID: "react-cursos-94ad2",
+    VITE_STORAGEBUCKET: "react-cursos-94ad2.appspot.com",
+    VITE_MESSAGINGSENDERID: "1072432689680",
+    VITE_APPID: "1:1072432689680:web:a0b12773cbffe8dab91095",
   };
 };
